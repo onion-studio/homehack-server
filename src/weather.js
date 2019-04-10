1a61aadfe6ba3bfcdc26da190dd454111c2d6027
Kim Seungha <seungha.me@gmail.com>
2019-04-11 00:38:53 +0900

    날씨 정보 폴리싱

--- src/weather.js
+++ src/weather.js
@@ -15,13 +15,22 @@ const locationX = envService.LOCATION_X;
 const locationY = envService.LOCATION_Y;
 const numOfRows = '5000';
 
+function lastBaseDate() {
+  const currentTime = formatDate(new Date(), 'HHmm');
+  // NOTE: API 제공 시각이 0210 정도임을 고려
+  const date =
+    currentTime < '0215'
+      ? new Date(Date.now() - 1000 * 60 * 60 * 24)
+      : new Date();
+  return formatDate(date, 'YYYYMMDD');
+}
+
 router.get('/', async (req, res) => {
-  const date = formatDate(new Date(), 'YYYYMMDD');
-  const baseTime = formatDate(new Date(), 'HHmm');
   const params = new URLSearchParams();
   params.append('serviceKey', serviceKey);
-  params.append('base_date', date);
-  params.append('base_time', baseTime);
+  params.append('base_date', lastBaseDate());
+  // NOTE: base_time에 따라 반환되는 데이터가 달라져서 일단 0200 으로 고정
+  params.append('base_time', '0200');
   params.append('nx', locationX);
   params.append('ny', locationY);
   params.append('numOfRows', numOfRows);
